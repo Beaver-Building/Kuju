3bd171e5a57be0e036007c52b184fcac84364a3c
CharingJC <junczhu@outlook.com>
2021-10-19 15:15:14 +0800

    feat
    update project

---
+++ PubHub/publisher/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Messaging.WebPubSub;
+
+namespace publisher
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Length != 3) {
+                Console.WriteLine("Usage: publisher <connectionString> <hub> <message>");
+                return;
+            }
+            var connectionString = args[0];
+            var hub = args[1];
+            var message = args[2];
+
+            var serviceClient = new WebPubSubServiceClient(connectionString, hub);
+
+            // Send messages to all the connected clients
+            // You can also try SendToConnectionAsync to send messages to the specific connection
+            await serviceClient.SendToAllAsync(message);
+        }
+    }
+}
